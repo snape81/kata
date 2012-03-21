58510dc945b88434d4a8e4c158ddf914d328f76c
snape81 <claudio.starnoni@gmail.com>
2012-03-21 12:26:18 +0100

    solved kata 1 & 2

--- kata-001/src/main/java/FizzBuzz.java
+++ kata-001/src/main/java/FizzBuzz.java
@@ -1,4 +1,3 @@
-
 public class FizzBuzz {
 
     /**
@@ -9,11 +8,27 @@ public class FizzBuzz {
      *
      * @param number a number from 1 to 100
      * @return a String representing the number or the Strings "Fizz", "Buzz" and "FizzBuzz"
-     *         as described above
+     * as described above
      * @throws IllegalArgumentException if the number is not in the 1-100 range.
      */
-    public String toFizzBuzzString(int number) {
-        return ""; // TODO: implement me
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String FIZZBUZZ = FIZZ + BUZZ;
+
+
+    public String toFizzBuzzString(int number) throws IllegalArgumentException {
+        if ( number <1 || number > 100)
+            throw new IllegalArgumentException("Number is not between allowed range " + number);
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            return FIZZBUZZ;
+        } else if (number % 3 == 0) {
+            return FIZZ;
+        } else if (number % 5 == 0) {
+            return BUZZ;
+        } else {
+            return String.valueOf(number);
+        }
     }
 
 }
--- kata-002/src/main/java/SoftwareVersionComparator.java
+++ kata-002/src/main/java/SoftwareVersionComparator.java
@@ -1,4 +1,3 @@
-import java.security.SecureRandom;
 import java.util.Comparator;
 
 public class SoftwareVersionComparator implements Comparator<String> {
@@ -22,7 +21,26 @@ public class SoftwareVersionComparator implements Comparator<String> {
      */
     @Override
     public int compare(String version1, String version2) {
-        return 42; // TODO: implement me
+       if (version1.equals(version2)) return 0;
+       String[] version1Array = version1.split("\\.");
+       String[] version2Array = version2.split("\\.");
+       int length = version1Array.length < version2Array.length ? version1Array.length : version2Array.length;
+       for (int i=0; i<length; i++) {
+           if (!version1Array[i].equals(version2Array[i])) {
+               if ((Integer.valueOf(version1Array[i])) < (Integer.valueOf(version2Array[i]))) {
+                       return -1;
+               } else {
+                   return 1;
+               }
+              
+          } 
+       }
+      if (version1Array.length<version2Array.length) {
+          return -1;
+      } else {
+          return 1;
+      }
     }
+
 
 }
