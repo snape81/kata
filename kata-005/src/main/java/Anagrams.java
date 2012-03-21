e5355648d74a1eab84162f764426d0aeeb9a8753
snape81 <claudio.starnoni@gmail.com>
2012-03-21 18:47:06 +0100

    solved kat 5 without permutation ... maybe no factorial complexity????

--- kata-005/src/main/java/Anagrams.java
+++ kata-005/src/main/java/Anagrams.java
@@ -1,4 +1,8 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Anagrams {
 
@@ -11,7 +15,58 @@ public class Anagrams {
      */
 
     public Set<String> findAnagrams(String word) {
-        return null; //TODO: implement me
+        Set<String> resultSet = new TreeSet<String>();
+        try {
+            Properties prop = new Properties();
+            
+            prop.load(this.getClass().getResourceAsStream("common-passwords.txt"));
+
+            HashMap<Character,Integer> wordAnalysysMap = getAnalysysMap(word);
+
+            for (Object key : prop.keySet()) {
+                String chiaveAttuale = (String) key;
+                if (chiaveAttuale.length() == word.length() ) {
+                    HashMap<Character,Integer> currentAnalysysMap = getAnalysysMap(chiaveAttuale);
+                    if(!word.equals(chiaveAttuale) && hashEqualsWord(wordAnalysysMap,currentAnalysysMap)) {
+                        resultSet.add(chiaveAttuale);
+                    }
+                }
+            }
+            
+        } catch (IOException ignore) {
+        }
+
+        return resultSet;
     }
 
+    private HashMap<Character,Integer> getAnalysysMap(String word) {
+        HashMap<Character,Integer> mappaToFill = new HashMap<Character, Integer>();
+        for(int i = 0; i < word.length(); i++) {
+            int charCounter = 0;
+            if (!mappaToFill.containsKey(word.charAt(i))) {
+                for(int j = i; j<word.length(); j++) {
+                    if (word.charAt(j) == word.charAt(i)) {
+                        charCounter++;
+                    }
+                }
+                mappaToFill.put(word.charAt(i),charCounter);
+            }
+        }
+        return mappaToFill;
+    }
+    
+
+
+    private boolean hashEqualsWord(HashMap<Character,Integer> hashMap1,HashMap<Character,Integer> hashMap2) {
+        for (Character character : hashMap1.keySet()) {
+            if (!hashMap2.containsKey(character)) {
+               return false;
+            } else {
+                if (!hashMap1.get(character).equals(hashMap2.get(character))) {
+                   return false;
+                }
+            }
+        }
+        return true;
+    }
 }
